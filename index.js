e8776bf808351a2a8129edf56c4945a6a334e836
Yanina Martinez Villaverde <yaninamartinez340@gmail.com>
2022-08-23 13:26:07 -0400

    Se agregó calcularEdad

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let nombre = "Juan Pérez";
-let edad = 25;
+let edad = calcularEdad(2000);
 console.log("Hola " + nombre);
 console.log(nombre + " tiene " + edad + " años");
+
+function calcularEdad(anioNac){
+    return new Date().getFullYear() - anioNac;
+}
