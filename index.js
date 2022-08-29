b4644e48f0cd2569aec45d1eb59ce19d6d10234b
Yanina Martinez Villaverde <yaninamartinez340@gmail.com>
2022-08-29 13:50:03 -0400

    Se creó la función calcularSalario

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-let nombre = "Juan Pérez";
+let nombre = "Yanina Martínez";
 let edad = calcularEdad(2000);
 console.log("Hola " + nombre);
 console.log(nombre + " tiene " + edad + " años");
+console.log("Se le pagará: "+calcularSalario(16))
 
 function calcularEdad(anioNac){
     return new Date().getFullYear() - anioNac;
+}
+
+function calcularSalario(horasTrabajadas){
+    return horasTrabajadas * 20000
 }
